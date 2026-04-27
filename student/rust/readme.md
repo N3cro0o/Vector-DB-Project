7a0a9f96e592192cc525419ebfc3707a089a26c1
N3cro0o <calko.n3cro0o@gmail.com>
2026-04-27 03:04:17 +0200

    Added readme to rust lab 1 task
    Added error handling to lab 1 rust exercise

---
+++ student/rust/readme.md
@@ -0,0 +1,7 @@
+## Laboratorium - Rust
+
+W tym katalogu przygotowano przykładowy kod obsługujący bazę danych ChromaDB. Do poprawnego działania programu należy przygotować lokalną instancję bazy danych poprzez wpisanie komendy: 
+
+`chroma run --path ./lab1`
+
+Przykładowy program należy wpierw skompilować poprzez wpisanie komendy `cargo run` albo poprzez `cargo build` i uruchomienie pliku wykonywalnego w katalogu `target/debug`.
--- student/rust/src/main.rs
+++ student/rust/src/main.rs
@@ -4,23 +4,43 @@ use std::error::Error;
 #[tokio::main]
 async fn main() {
     let client = HttpClient::new(Default::default());
-    let collection = client.create_collection("my_collection", None, None).await.unwrap();
+    let collection = match client.create_collection("my_collection", None, None).await {
+        Ok(c) => c,
+        Err(err) => {
+            eprintln!("Error while getting collection: {}", err);
+            return;
+        }
+    };
 
     let embeddings = vec![vec![0.1, 0.2, 0.3], vec![0.4, 0.5, 0.6]];
-    collection
-    .add(vec!["id1".to_string(), "id2".to_string()],
-        embeddings,
-        Some(vec![
-                Some("This is a document about pineapple".to_string()),
-                Some("This is a document about oranges".to_string()),
-            ]),
-        None,
-        None,)
-        .await.unwrap();
-    let results = collection
+    match collection
+        .add(vec!["id1".to_string(), "id2".to_string()],
+            embeddings,
+            Some(vec![
+                    Some("This is a document about pineapple".to_string()),
+                    Some("This is a document about oranges".to_string()),
+                ]),
+            None,
+            None,
+        ).await {
+    Ok(_) => println!("Inserted items successfuly."),
+    Err(err) => {
+            eprintln!("Error while inserting to collection: {}", err);
+            return;
+        }
+    }
+    let results = match collection
         .query(vec![vec![0.1, 0.2, 0.3]], Some(2), None, None, None)
-        .await.unwrap();
+        .await {
+        Ok(r) => r,
+        Err(err) => {
+            eprintln!("Error while getting results from collection: {}", err);
+            return;
+        }
+    };
     println!("Result: {:#?}", results);
-    let collection = client.delete_collection("my_collection").await.unwrap();
+    if let Err(err) = client.delete_collection("my_collection").await {
+        eprintln!("Error while deleting collection: {}", err);
+    }
 }
 
